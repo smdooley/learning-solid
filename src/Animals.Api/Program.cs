a42951543ce4ca37fe568859513f804802285f71
smdooley <sean.dooley.22@gmail.com>
2025-09-03 14:50:13 +0000

    Enhance API to serve animal data; update Animal class to expose Name property

--- src/Animals.Api/Program.cs
+++ src/Animals.Api/Program.cs
@@ -1,37 +1,59 @@
+using Animals.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Launch the app and navigate to https://localhost:<port>/openapi/v1.json to view the generated OpenAPI document.
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
+
+var animals = new List<Animals.Domain.Animal>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Animals.Domain.Cat("Whiskers"),
+    new Animals.Domain.Dog("Buddy"),
+    new Animals.Domain.Sheep("Dolly"),
+    new Animals.Domain.Cow("Bessie")
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/animals", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var result = animals.ToArray();
+
+    return result;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetAnimals");
 
 app.Run();
 
--- src/Animals.Domain/Animal.cs
+++ src/Animals.Domain/Animal.cs
@@ -4,12 +4,10 @@ namespace Animals.Domain
 {
     public abstract class Animal
     {
-        // public Guid Id { get; init; }
-        protected string Name { get; set; }
+        public string Name { get; protected set; }
 
         protected Animal(string name)
         {
-            // Id = id == Guid.Empty ? Guid.NewGuid() : id;
             Name = string.IsNullOrWhiteSpace(name) 
                 ? throw new ArgumentException("Name cannot be empty.", nameof(name)) 
                 : name.Trim();
